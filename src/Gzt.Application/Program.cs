604c50b341eadfd1fc29d2d670646aa44ea5b5aa
Marcos Paulo Honorato da Silva <pauloabejr@hotmail.com>
2017-02-21 18:06:41 -0300

    new

---
+++ src/Gzt.Application/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gzt.Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { }
+    }
+}
---
+++ src/Gzt.Infra.CrossCutting.Bus/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gzt.Infra.CrossCutting.Bus
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { }
+    }
+}
